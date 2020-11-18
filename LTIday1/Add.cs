819223c79fa5b922df1f5cca7dd98c68a85fa6a9
jumandas <dasjuman9@gmail.com>
2020-11-18 12:13:08 +0530

    C#Project

---
+++ LTIday1/Add.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LTIday1
+{
+    class Add
+    {
+        static void Main()
+        {
+            int no1 = 30, no2 = 20,Result;
+            Result = no1 + no2;
+            Console.WriteLine("Result is {0}", Result);
+            Console.ReadKey();
+        }
+    }
+}
